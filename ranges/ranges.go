971517ab58c23e1df0f2f928d1d335654dbb62c6
Justin Israel <justinisrael@gmail.com>
2015-08-23 10:50:08 +1200

    wip - ranges package

---
+++ ranges/ranges.go
@@ -0,0 +1,518 @@
+/*
+Package ranges provides objects that can represent
+simple integer ranges with an optional stepping, as
+well as complex non-contiguous ranges.
+
+Simple range:
+
+    1-10
+    20-100x3
+    -10-50
+
+Complex ranges:
+
+    1-10,20-40x2,30,80-100x3
+
+*/
+package ranges
+
+import (
+	"bytes"
+	"fmt"
+	"math"
+	"strconv"
+)
+
+// Iterator is an interface type that can
+// iterate over int values.
+type Iterator interface {
+	// The Iterator has another valid value
+	// to produce, while IsDone() returns true
+	IsDone() bool
+	// Next retrieves the next available value,
+	// and advances the Iterator. It is only
+	// valid to call Next() while IsDone()
+	// returns true
+	Next() int
+}
+
+// InclusiveRange represents a start and end
+// value, and a stepping amount between each
+// value in the range. The range includes the end value.
+// It dynamically acts like a slice of values
+// in an integer range, without needing to store
+// the discreet values. So it can represent very
+// large ranges.
+type InclusiveRange struct {
+	start int
+	end   int
+	step  int
+
+	cachedEnd   int
+	isEndCached bool
+
+	cachedLen   int
+	isLenCached bool
+}
+
+// NewInclusiveRange creates a new InclusiveRange instance
+func NewInclusiveRange(start, end, step int) *InclusiveRange {
+	if step == 0 {
+		if start <= end {
+			step = 1
+
+		} else {
+			step = -1
+		}
+	}
+
+	r := &InclusiveRange{
+		start: start,
+		end:   end,
+		step:  step,
+	}
+	return r
+}
+
+// String returns a formatted string representation
+// of the integer range
+func (r *InclusiveRange) String() string {
+	var buf bytes.Buffer
+
+	// Always for a single value
+	buf.WriteString(strconv.Itoa(r.Start()))
+
+	// If we have a range, express the end value
+	if r.End() != r.Start() {
+		buf.WriteString(`-`)
+		buf.WriteString(strconv.Itoa(r.End()))
+
+		// Express the stepping, if its not 1
+		step := r.Step()
+		if step > 1 || step < -1 {
+			buf.WriteString(`x`)
+			buf.WriteString(strconv.Itoa(r.Step()))
+		}
+	}
+	return buf.String()
+}
+
+// Start returns the start of the range
+func (r *InclusiveRange) Start() int {
+	return r.start
+}
+
+// End returns the end of the range. This value may
+// be different from the end value given when the
+// range was first initialized, since it takes into
+// account the stepping value. The end value may be
+// shifted to the closest valid value within the
+// stepped range.
+func (r *InclusiveRange) End() int {
+	if r.isEndCached {
+		return r.cachedEnd
+	}
+
+	r.isEndCached = true
+
+	// If we aren't stepping, or we don't have
+	// a full range, then just use the end value
+	if r.step == 1 || r.step == -1 || r.start == r.end {
+		r.cachedEnd = r.end
+		return r.cachedEnd
+	}
+
+	// If the step is in the wrong direction,
+	// compared to the range direction, then
+	// just use the start as the end.
+	if (r.end < r.start) && r.step < (r.end-r.start) {
+		r.cachedEnd = r.start
+		return r.cachedEnd
+
+	} else if (r.end > r.start) && r.step > (r.end-r.start) {
+		r.cachedEnd = r.start
+		return r.cachedEnd
+	}
+
+	// Calculate the end, taking into account the stepping
+	r.cachedEnd = r.closestInRange(r.end, r.start, r.end, r.step)
+	return r.cachedEnd
+}
+
+// Step returns the stepping value used in the range
+func (r *InclusiveRange) Step() int {
+	return r.step
+}
+
+// Len returns the number of values in the range
+func (r *InclusiveRange) Len() int {
+	if r.isLenCached {
+		return r.cachedLen
+	}
+
+	// Offset by one to include the end value
+	diff := math.Abs(float64(r.end-r.start)) + 1
+	r.cachedLen = int(math.Ceil(diff / math.Abs(float64(r.step))))
+	r.isLenCached = true
+	return r.cachedLen
+}
+
+// Contains returns true if the given value is a valid
+// value within the value range.
+func (r *InclusiveRange) Contains(value int) bool {
+	// If we attempt to find the closest value, given
+	// the start of the range and the step, we can check
+	// if it is still the same number. If it hasn't changed,
+	// then it is in the range.
+	closest := r.closestInRange(value, r.start, r.End(), r.step)
+	return closest == value
+}
+
+// closestInRange finds the closest valid value within the range,
+// to a given value. Values outside the range are clipped to either
+// the range min or max.
+func (*InclusiveRange) closestInRange(value, start, end, step int) int {
+	// Possibly clamp the value if it is outside the range
+	if end >= start {
+		if value < start {
+			return start
+		} else if value > end {
+			return end
+		}
+
+	} else {
+		if value > start {
+			return start
+		} else if value < end {
+			return end
+		}
+	}
+
+	// No calculation needed if there is no stepping
+	if step == 1 || step == -1 {
+		return value
+	}
+
+	// Modified the value so that it is a properly stepped
+	// increment within the range
+	return (((value - start) / step) * step) + start
+}
+
+// Value returns the value at the given index in
+// the range. If the index is invalid/exceeds the valid
+// range, an error will be returned.
+func (r *InclusiveRange) Value(idx int) (int, error) {
+	if idx < 0 {
+		return 0, fmt.Errorf("Index %d is not >= 0", idx)
+	}
+
+	// Calculate the value as an offset from the start
+	start := r.Start()
+	end := r.End()
+	step := r.Step()
+
+	val := start + (step * idx)
+
+	if start <= end && (val < start || val > end) {
+		return 0, fmt.Errorf("Index %d exceeds max index of %d", idx, r.Len()-1)
+
+	} else if end < start && (val > start || val < end) {
+		return 0, fmt.Errorf("Index %d exceeds max index of %d", idx, r.Len()-1)
+	}
+
+	return val, nil
+}
+
+// Index returns the 0-based index of the first occurrence
+// given value, within the range.
+// If the value does not exist in the range, a
+// value of -1 will be returned
+func (f *InclusiveRange) Index(value int) int {
+	closest := f.closestInRange(value, f.start, f.End(), f.step)
+	if closest != value {
+		return -1
+	}
+	idx := (value - f.start) / f.step
+	if idx < 0 {
+		idx *= -1
+	}
+	return idx
+}
+
+// IterValues returns an iterator that will loop through
+// every value in the range.
+// Iterator is valid while IsDone() returns false.
+// Next value on valid iterator can be retrieved via Next()
+func (f *InclusiveRange) IterValues() Iterator {
+	return &inclusiveRangeIt{ptr: f}
+}
+
+// tracks the iteration state across an inclusiveRange instance
+type inclusiveRangeIt struct {
+	ptr *InclusiveRange
+	pos int
+}
+
+// IsDone returns false while the iterator is valid
+// and can produce another value, via a call to Next()
+func (it *inclusiveRangeIt) IsDone() bool {
+	if it.pos >= it.ptr.Len() {
+		return true
+	}
+	return false
+}
+
+// Next() returns the current value in the
+// iteration, and advances the interator. Next()
+// will produce a valid result as long as a call
+// to isDone() returns false.
+func (it *inclusiveRangeIt) Next() int {
+	val, err := it.ptr.Value(it.pos)
+	it.pos++
+	if err != nil {
+		return it.ptr.End()
+	}
+	return val
+}
+
+// InclusiveRanges is able to represent multiple non-contiguous
+// ranges, with non-uniform stepping, ie. 1-10,20-30x2,100,120
+// Because range values are evaluated dynamically as
+// offsets within the start/stop, they can represent
+// very large ranges.
+// The zero value is a ready to use instance.
+type InclusiveRanges struct {
+	blocks []*InclusiveRange
+}
+
+// String returns the formatted representation of
+// the combination of all internal InclusiveRange instances
+func (l *InclusiveRanges) String() string {
+	var buf bytes.Buffer
+	for i, b := range l.blocks {
+		if i > 0 {
+			buf.WriteString(`,`)
+		}
+		buf.WriteString(b.String())
+	}
+	return buf.String()
+}
+
+// Len returns the total number of values across all ranges
+func (l *InclusiveRanges) Len() int {
+	var totalLen int
+	for _, b := range l.blocks {
+		totalLen += b.Len()
+	}
+	return totalLen
+}
+
+// Start returns the first value of the first range
+func (l *InclusiveRanges) Start() int {
+	for _, b := range l.blocks {
+		return b.Start()
+	}
+	return 0
+}
+
+// End returns the last value of the last range
+func (l *InclusiveRanges) End() int {
+	if l.blocks == nil {
+		return 0
+	}
+	return l.blocks[len(l.blocks)-1].End()
+}
+
+// NumRanges returns the number of discreet sets
+// of ranges that were appended.
+func (l *InclusiveRanges) numRanges() int {
+	if l.blocks == nil {
+		return 0
+	}
+	return len(l.blocks)
+}
+
+// rangeAt returns the underlying InclusiveRange instance
+// that was appended, at a given index
+func (l *InclusiveRanges) rangeAt(idx int) *InclusiveRange {
+	if idx < 0 || idx >= l.numRanges() {
+		return nil
+	}
+	return l.blocks[idx]
+}
+
+// Append creates and adds another range of values
+// to the total range list.
+func (l *InclusiveRanges) Append(start, end, step int) {
+	block := NewInclusiveRange(start, end, step)
+	l.blocks = append(l.blocks, block)
+}
+
+// AppendUnique creates and adds another range of values
+// to the total range list. Only unique values from the
+// given range are appended to the total range.
+func (l *InclusiveRanges) AppendUnique(start, end, step int) {
+	subStart := start
+	subEnd := start
+	subStep := step
+	last := start
+	pending := 0 // Track unique value count
+
+	// Handle loop test for both increasing
+	// and decreasing ranges
+	var pred func() bool
+	if start <= end {
+		pred = func() bool { return subEnd <= end }
+	} else {
+		pred = func() bool { return subEnd >= end }
+	}
+
+	for ; pred(); subEnd += step {
+		if !l.Contains(subEnd) {
+			// Is a unique value in the range
+			last = subEnd
+			if pending == 0 {
+				subStart = last
+			}
+			pending++
+			continue
+		}
+
+		if pending == 0 {
+			// Nothing to add yet
+			continue
+		}
+
+		// Current value is already in range.
+		// Add previous values
+		l.Append(subStart, last, subStep)
+		subStart = subEnd + step
+		pending = 0
+	}
+
+	// Flush the remaining values
+	if pending > 0 {
+		l.Append(subStart, last, subStep)
+	}
+}
+
+// Contains returns true if a given value is a valid
+// value within the total range.
+func (l *InclusiveRanges) Contains(value int) bool {
+	for _, b := range l.blocks {
+		if b.Contains(value) {
+			return true
+		}
+	}
+	return false
+}
+
+// Value returns the value at the given index in
+// the total range. If the index is invalid/exceeds the valid
+// range, an error will be returned.
+func (l *InclusiveRanges) Value(idx int) (int, error) {
+	if idx < 0 {
+		return 0, fmt.Errorf("Index %d is not >= 0", idx)
+	}
+
+	var err error
+	var val, size, n int
+
+	for _, b := range l.blocks {
+		size = b.Len()
+
+		// modify the original index to be within
+		// the range of the current range block, and
+		// then check that it is a valid index value
+		if (idx - n) < size {
+			if val, err = b.Value(idx - n); err == nil {
+				return val, nil
+			}
+		}
+
+		// record the offset so we can continue to
+		// modify the original index value to the local range
+		n += size
+	}
+
+	// The previous loop ended in error
+	return 0, fmt.Errorf("Index %d exceeds the total range", idx)
+}
+
+// Index returns the 0-based index of the first occurrence
+// of the given value, within the range.
+// If the value does not exist in the range, a
+// value of -1 will be returned.
+func (l *InclusiveRanges) Index(value int) int {
+	var idx, n int
+
+	for _, b := range l.blocks {
+		// If the value is within the current block
+		// then return the local index, offset by the
+		// number of previous values we have tracked
+		if idx = b.Index(value); idx >= 0 {
+			return idx + n
+		}
+		// Update the offset for the values we have seen
+		n += b.Len()
+	}
+
+	// The previous loop ended in error
+	return -1
+}
+
+// IterValues returns an iterator that will loop through
+// every value in the total range.
+// Iterator is valid while IsDone() returns false.
+// Next value on valid iterator can be retrieved via Next()
+func (l *InclusiveRanges) IterValues() Iterator {
+	return &inclusiveRangesIt{ptr: l}
+}
+
+// tracks the iteration state across a inclusiveRanges instance
+type inclusiveRangesIt struct {
+	ptr     *InclusiveRanges
+	current Iterator
+	pos     int
+}
+
+func (it *inclusiveRangesIt) init() bool {
+	if it.current == nil {
+		block := it.ptr.rangeAt(it.pos)
+		if block == nil {
+			return false
+		}
+		it.current = block.IterValues()
+	}
+	return true
+}
+
+// IsDone returns false while the iterator is valid
+// and can produce another value, via a call to Next()
+func (it *inclusiveRangesIt) IsDone() bool {
+	if !it.init() {
+		return true
+	}
+
+	return it.current.IsDone()
+}
+
+// Next() returns the current value in the
+// iteration, and advances the interator. Next()
+// will produce a valid result as long as a call
+// to isDone() returns false.
+func (it *inclusiveRangesIt) Next() int {
+	if !it.init() {
+		return it.ptr.End()
+	}
+
+	val := it.current.Next()
+
+	if it.current.IsDone() {
+		// Get the next available range
+		it.pos++
+		it.current = nil
+	}
+
+	return val
+}
---
+++ ranges/ranges_test.go
@@ -0,0 +1,278 @@
+package ranges
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestRangeInit(t *testing.T) {
+	table := []struct {
+		start, end, step int
+		rEnd, rLen       int
+		rStr             string
+	}{
+		{1, 10, 1,
+			10, 10, "1-10"}, // 0
+		{10, 1, -1,
+			1, 10, "10-1"}, // 1
+		{-10, 20, 1,
+			20, 31, "-10-20"}, // 2
+		{20, -10, -1,
+			-10, 31, "20--10"}, // 3
+
+		{1, 10, 3,
+			10, 4, "1-10x3"}, // 4
+		{10, -10, -3,
+			-8, 7, "10--8x-3"}, // 5
+
+		{-100, -33, 7,
+			-37, 10, "-100--37x7"}, // 6
+		{-33, -100, -7,
+			-96, 10, "-33--96x-7"}, // 7
+
+		{1, 10, 0,
+			10, 10, "1-10"}, // 8
+		{10, 1, 0,
+			1, 10, "10-1"}, // 9
+	}
+
+	var actual int
+	for i, tt := range table {
+		r := NewInclusiveRange(tt.start, tt.end, tt.step)
+		actual = r.Start()
+		if actual != tt.start {
+			t.Errorf("Test %d: Expected start %d; got %d", i, tt.start, actual)
+		}
+		actual = r.End()
+		if actual != tt.rEnd {
+			t.Errorf("Test %d: Expected end %d; got %d", i, tt.rEnd, actual)
+		}
+		actual = r.Step()
+		expected := tt.step
+		if expected == 0 {
+			if r.End() >= r.Start() {
+				expected = 1
+			} else {
+				expected = -1
+			}
+		}
+		if actual != expected {
+			t.Errorf("Test %d: Expected step %d; got %d", i, expected, actual)
+		}
+		actual = r.Len()
+		if actual != tt.rLen {
+			t.Errorf("Test %d: Expected len %d; got %d", i, tt.rLen, actual)
+		}
+		actualStr := r.String()
+		if actualStr != tt.rStr {
+			t.Errorf("Test %d: Expected %q; got %q", i, tt.rStr, actualStr)
+		}
+	}
+}
+
+func TestRangeLookups(t *testing.T) {
+	table := []struct {
+		start, end, step int
+		indexToValue     map[int]int
+		valueToIndex     map[int]int
+	}{
+		// 0
+		{1, 10, 1,
+			map[int]int{0: 1, 5: 6, 9: 10},
+			map[int]int{1: 0, 6: 5, 10: 9, -1: -1, 11: -1}},
+		// 1
+		{10, -10, -3,
+			map[int]int{0: 10, 3: 1, 6: -8},
+			map[int]int{10: 0, 1: 3, -8: 6, 3: -1, -7: -1}},
+	}
+
+	for i, tt := range table {
+		r := NewInclusiveRange(tt.start, tt.end, tt.step)
+
+		for idx, expected := range tt.indexToValue {
+			actual, err := r.Value(idx)
+			if err != nil {
+				t.Errorf("Test %d: Failed Value(idx): %s", err.Error())
+				continue
+			}
+			if actual != expected {
+				t.Errorf("Test %d: Expected value %d for index %d; got %d",
+					i, expected, idx, actual)
+			}
+		}
+
+		for val, expected := range tt.valueToIndex {
+			actual := r.Index(val)
+			if actual != expected {
+				t.Errorf("Test %d: Expected index %d for value %d; got %d",
+					i, expected, val, actual)
+			}
+		}
+	}
+}
+
+func TestRangeIterValues(t *testing.T) {
+	table := []struct {
+		start, end, step int
+		expected         []int
+	}{
+		// 0
+		{-5, 5, 1,
+			[]int{-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5}},
+		// 1
+		{-10, 10, 3,
+			[]int{-10, -7, -4, -1, 2, 5, 8}},
+		// 2
+		{20, -30, -7,
+			[]int{20, 13, 6, -1, -8, -15, -22, -29}},
+	}
+
+	for i, tt := range table {
+		r := NewInclusiveRange(tt.start, tt.end, tt.step)
+		var actual []int
+		for it := r.IterValues(); !it.IsDone(); {
+			actual = append(actual, it.Next())
+		}
+		if !reflect.DeepEqual(actual, tt.expected) {
+			t.Errorf("Test %d: got %v; expected %v", i, actual, tt.expected)
+		}
+	}
+}
+
+func TestRangesInit(t *testing.T) {
+	table := []struct {
+		params     [][]int
+		rEnd, rLen int
+		rStr       string
+	}{
+		// 0
+		{
+			[][]int{},
+			0, 0, "",
+		},
+		// 1
+		{
+			[][]int{{1, 10, 1}, {13, 14, 1}, {17, 17, 1}, {1, 30, 3}},
+			28, 18, "1-10,13-14,17,16-28x3",
+		},
+		// 2
+		{
+			[][]int{{-5, 5, 2}, {10, 10, 1}, {-10, 10, 1}},
+			9, 21, "-5-5x2,10,-10--6,-4,-2,0,2,4,6-9",
+		},
+	}
+
+	var actual int
+	for i, tt := range table {
+		var r InclusiveRanges
+		for _, p := range tt.params {
+			r.AppendUnique(p[0], p[1], p[2])
+		}
+		var start int
+		if len(tt.params) > 0 {
+			start = tt.params[0][0]
+		}
+
+		actual = r.Start()
+		if actual != start {
+			t.Errorf("Test %d: Expected start %d; got %d", i, tt.params[0], actual)
+		}
+		actual = r.End()
+		if actual != tt.rEnd {
+			t.Errorf("Test %d: Expected end %d; got %d", i, tt.rEnd, actual)
+		}
+		actual = r.Len()
+		if actual != tt.rLen {
+			t.Errorf("Test %d: Expected len %d; got %d", i, tt.rLen, actual)
+		}
+		actualStr := r.String()
+		if actualStr != tt.rStr {
+			t.Errorf("Test %d: Expected %q; got %q", i, tt.rStr, actualStr)
+		}
+	}
+}
+
+func TestRangesLookup(t *testing.T) {
+	table := []struct {
+		params       [][]int
+		indexToValue map[int]int
+		valueToIndex map[int]int
+	}{
+		// 0
+		{
+			[][]int{{1, 10, 1}, {13, 14, 1}, {17, 17, 1}, {1, 30, 3}},
+			map[int]int{0: 1, 10: 13, 12: 17, 17: 28},
+			map[int]int{1: 0, 13: 10, 17: 12, 28: 17, -1: -1, 15: -1, 30: -1},
+		},
+		// 1
+		{
+			[][]int{{-5, 5, 2}, {10, 10, 1}, {-10, 10, 1}},
+			map[int]int{0: -5, 5: 5, 6: 10, 11: -6, 20: 9},
+			map[int]int{-5: 0, 5: 5, 10: 6, -6: 11, 9: 20, -11: -1, 11: -1},
+		},
+	}
+
+	for i, tt := range table {
+		var r InclusiveRanges
+		for _, p := range tt.params {
+			r.AppendUnique(p[0], p[1], p[2])
+		}
+
+		for idx, expected := range tt.indexToValue {
+			actual, err := r.Value(idx)
+			if err != nil {
+				t.Errorf("Test %d: Failed Value(idx): %s", i, err.Error())
+				continue
+			}
+			if actual != expected {
+				t.Errorf("Test %d: Expected value %d for index %d; got %d",
+					i, expected, idx, actual)
+			}
+		}
+
+		for val, expected := range tt.valueToIndex {
+			actual := r.Index(val)
+			if actual != expected {
+				t.Errorf("Test %d: Expected index %d for value %d; got %d",
+					i, expected, val, actual)
+			}
+		}
+	}
+}
+
+func TestRangesIterValues(t *testing.T) {
+	table := []struct {
+		params   [][]int
+		expected []int
+	}{
+		{ // 0
+			[][]int{{-5, 5, 2}, {10, 10, 1}, {-10, 10, 1}},
+			[]int{
+				-5, -3, -1, 1, 3, 5, 10, -10, -9, -8,
+				-7, -6, -4, -2, 0, 2, 4, 6, 7, 8, 9,
+			},
+		},
+		{ // 1
+			[][]int{{-3, 3, 1}, {-5, 5, 1}},
+			[]int{-3, -2, -1, 0, 1, 2, 3, -5, -4, 4, 5},
+		},
+		{ // 2
+			[][]int{{10, 1, -2}, {-5, 10, 2}},
+			[]int{10, 8, 6, 4, 2, -5, -3, -1, 1, 3, 5, 7, 9},
+		},
+	}
+
+	for i, tt := range table {
+		var r InclusiveRanges
+		for _, p := range tt.params {
+			r.AppendUnique(p[0], p[1], p[2])
+		}
+		var actual []int
+		for it := r.IterValues(); !it.IsDone(); {
+			actual = append(actual, it.Next())
+		}
+		if !reflect.DeepEqual(actual, tt.expected) {
+			t.Errorf("Test %d: got %v; expected %v", i, actual, tt.expected)
+		}
+	}
+}
