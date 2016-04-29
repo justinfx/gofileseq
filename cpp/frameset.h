6323068d7ae229e52106f5c0ffac246b58eb3919
Justin Israel <justinisrael@gmail.com>
2016-04-29 14:45:27 +1200

    tabs to spaces fix

--- cpp/frameset.h
+++ cpp/frameset.h
@@ -11,33 +11,33 @@ namespace fileseq {
 class FrameSet {
 
 public:
-	explicit FrameSet(const std::string &frange);
-	~FrameSet();
+    explicit FrameSet(const std::string &frange);
+    ~FrameSet();
 
     FrameSet(const FrameSet& rhs);
     FrameSet& operator=(const FrameSet& rhs);
 
-	bool isValid() const { return m_valid; }
+    bool isValid() const { return m_valid; }
     std::string getLastError() const { return m_lastError; }
 
-	std::string string() const;
-	size_t length() const;
-	int index(int frame) const;
+    std::string string() const;
+    size_t length() const;
+    int index(int frame) const;
     int frame(int index, bool* ok=NULL) const;
     void frames(std::vector<int> &frames) const;
-	bool hasFrame(int frame) const;
-	int start() const;
-	int end() const;
-	std::string frameRange(int pad=0) const;
-	FrameSet inverted() const;
-	std::string invertedFrameRange(int pad) const;
-	FrameSet normalized() const;
+    bool hasFrame(int frame) const;
+    int start() const;
+    int end() const;
+    std::string frameRange(int pad=0) const;
+    FrameSet inverted() const;
+    std::string invertedFrameRange(int pad) const;
+    FrameSet normalized() const;
 
 private:
     FrameSet(GoInt64 id);
 
-	bool m_valid;
-	GoInt64 m_id;
+    bool m_valid;
+    GoInt64 m_id;
 
     mutable std::string m_lastError;
 };
