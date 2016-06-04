d42fd2fe1ffbfa796979039f24f385001f5b51c1
Justin Israel <jisrael@wetafx.co.nz>
2016-06-04 18:00:23 +1200

    Full gtest stubs to match gofileseq tests, with TODO failure placeholders

--- cpp-port/test/TestFrameSet.cc
+++ cpp-port/test/TestFrameSet.cc
@@ -1,5 +1,23 @@
+#include "common.h"
 #include <gtest/gtest.h>
 
-TEST( TestFrameSet, TODO ) {
+TEST( TestFrameSet, New ) {
+    TODO();
+};
 
-}
+TEST( TestFrameSet, Lookup ) {
+    TODO();
+};
+
+TEST( TestFrameSet, Padding ) {
+    TODO();
+};
+
+TEST( TestFrameSet, Normalize ) {
+    TODO();
+};
+
+TEST( TestFrameSet, Inverted ) {
+    TODO();
+};
+
---
+++ cpp-port/test/common.h
@@ -0,0 +1,6 @@
+#ifndef FILESEQ_CPP_GTEST_COMMON_H
+#define FILESEQ_CPP_GTEST_COMMON_H
+
+#define TODO() FAIL() << "Not Implemented"
+
+#endif
