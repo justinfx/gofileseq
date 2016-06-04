96d29e6d32dcddcf0cca36501b628c08d9f25c8e
Justin Israel <jisrael@wetafx.co.nz>
2016-06-04 18:00:23 +1200

    Full gtest stubs to match gofileseq tests, with TODO failure placeholders

--- cpp-port/test/TestFileSequence.cc
+++ cpp-port/test/TestFileSequence.cc
@@ -1,5 +1,23 @@
+#include "common.h"
 #include <gtest/gtest.h>
 
-TEST( TestFileSequence, TODO ) {
-
+TEST( TestFileSequence, New ) {
+    TODO();
 }
+
+TEST( TestFileSequence, Split ) {
+    TODO();
+}
+
+TEST( TestFileSequence, FrameIndex ) {
+    TODO();
+}
+
+TEST( TestFileSequence, SetDir ) {
+    TODO();
+}
+
+TEST( TestFileSequence, Format ) {
+    TODO();
+}
+
---
+++ cpp-port/test/TestFunctions.cc
@@ -0,0 +1,30 @@
+#include "common.h"
+#include <gtest/gtest.h>
+
+TEST( TestFileSequence, FramesToFrameRange ) {
+    TODO();
+}
+
+TEST( TestFileSequence, PaddingChars ) {
+    TODO();
+}
+
+TEST( TestFileSequence, PaddingCharsSize ) {
+    TODO();
+}
+
+TEST( TestFileSequence, ToRange ) {
+    TODO();
+}
+
+TEST( TestFileSequence, FindSequencesOnDisk ) {
+    TODO();
+}
+
+TEST( TestFileSequence, FindSequenceOnDisk ) {
+    TODO();
+}
+
+TEST( TestFileSequence, HandleSymlinksOnDisk ) {
+    TODO();
+}
