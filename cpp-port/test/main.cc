e43c724be466f2b6735ec9631caee6a254a5c747
Justin Israel <jisrael@wetafx.co.nz>
2016-06-07 18:45:12 +1200

    gtest binary should change directory to source location when running

--- cpp-port/test/main.cc
+++ cpp-port/test/main.cc
@@ -1,6 +1,21 @@
 #include <gtest/gtest.h>
 
+#include <cstdlib>
+#include <cstring>
+#include <unistd.h>
+
+#define xstr(a) str(a)
+#define str(a) #a
+
 int main( int argc, char** argv ) {
+
+    #ifdef FILESEQ_TEST_DIR
+    int ret = chdir(xstr(FILESEQ_TEST_DIR));
+    if (ret != 0) {
+        perror("Failed to change dir to: " xstr(FILESEQ_TEST_DIR));
+        exit(1);
+    }
+    #endif
 
     testing::InitGoogleTest( &argc, argv );
 
